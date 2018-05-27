39f5148cf862a7448e745b8ac4f738b78d57093b
azole <azolelai@gmail.com>
2018-05-28 06:48:45 +0800

    example for 2.6.1

--- tests/Chap2/LogAnalyzerTest.php
+++ tests/Chap2/LogAnalyzerTest.php
@@ -12,14 +12,26 @@ use PHPUnit\Framework\TestCase;
 
 final class LogAnalyzerTest extends TestCase
 {
+    private $analyzer;
+
+    // AUT作者建議：不要用 setUp 來初始化被測試的類別，建議採用工廠方法
+    protected function setUp()
+    {
+        $this->analyzer = new \Aut\Chap2\LogAnalyzer();
+    }
+
+    protected function tearDown()
+    {
+        // 反模式，實務上不要這樣做
+        $this->analyzer = null;
+    }
+
     /**
      * @test
      */
     public function isValidFileName_BadExtension_ReturnFalse()
     {
-        $analyzer = new \Aut\Chap2\LogAnalyzer();
-
-        $result = $analyzer->isValidLogFileName("file_with_bad_extension.foo");
+        $result = $this->analyzer->isValidLogFileName("file_with_bad_extension.foo");
 
         $this->assertFalse($result);
     }
@@ -35,9 +47,7 @@ final class LogAnalyzerTest extends TestCase
      */
     public function isValidLogFileName_ValidExtensions_ReturnTrue($filename)
     {
-        $analyzer = new \Aut\Chap2\LogAnalyzer();
-
-        $result = $analyzer->isValidLogFileName($filename);
+        $result = $this->analyzer->isValidLogFileName($filename);
 
         $this->assertTrue($result);
     }
